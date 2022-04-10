ba5a1e0792b953a1317fd85ed4e8c21d8c5649df
athlazara <41614380+athlazara@users.noreply.github.com>
2022-04-10 15:57:37 +0800

    Update soul_tide_5055.py

--- soul_tide_5055.py
+++ soul_tide_5055.py
@@ -130,4 +130,4 @@ if lines:
     #df.style.hide_index() 隐藏序号无效
     st.dataframe(df.style.set_properties(**{'text-align': 'center'}), width=1000, height=310)
 else:
-    st.markdown("<font color=red size=10>**_无整数消耗路径！_**</font>"+':ghost:', unsafe_allow_html=True)
+    st.markdown("<font face="微软雅黑" color=HotPink size=5>**_无整数消耗路径！_**</font>"+':ghost:', unsafe_allow_html=True)
