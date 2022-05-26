bfd9faa3723993dc92cd93fcbbbfe5023e1f57db
athlazara <41614380+athlazara@users.noreply.github.com>
2022-05-27 00:42:27 +0800

    Update opentrons_test.py

--- opentrons_test.py
+++ opentrons_test.py
@@ -47,9 +47,8 @@ with col1:
     else:
         st.button('复位', disabled=True)
 with col2:
-    with open(path, "rb") as file:
-        st.download_button(
-            label="下载定位文件",
-            data=file,
-            file_name="calibrations.json",
-            mime="json")
+    st.download_button(
+        label="下载定位文件",
+        data=path,
+        file_name="calibrations.json",
+        mime="json")
