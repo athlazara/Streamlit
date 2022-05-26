a6cc54e812dee9c87acc25ee7a86553f0b299c12
athlazara <41614380+athlazara@users.noreply.github.com>
2022-05-26 22:16:24 +0800

    Update opentrons_test.py

--- opentrons_test.py
+++ opentrons_test.py
@@ -1,55 +1,55 @@
 import streamlit as st
-from opentrons import containers, instruments, robot
+from PIL import Image
+from opentrons import robot, containers, instruments
+from opentrons.util import environment
 
-robot.head_speed(x=12000, y=12000, z=4000, a=600, b=600)
-tuberack1 = containers.load('tube-rack-2ml', 'C1')
-tuberack2 = containers.load('tube-rack-2ml', 'D1')
-tuberack3 = containers.load('tube-rack-2ml', 'C2')
-tuberack4 = containers.load('tube-rack-2ml', 'D2')
+switch = False
+ports = ['COM1']#robot.get_serial_ports_list()
+ports += ['断开连接']
+red = Image.open(r'https://github.com/athlazara/Streamlit/red circle.jpg')
+green = Image.open(r'https://github.com/athlazara/Streamlit/green circle.jpg')
 
-containers.create(
-    'bottle',             # name of you container
-    grid=(1, 1),                   # specify amount of (columns, rows)
-    spacing=(60, 60),               # distances (mm) between each (column, row)
-    diameter=50,                    # diameter (mm) of each well on the EPplate
-    depth=60)                       # depth (mm) of each well on the EPplate
-trough = containers.load('bottle','A2') # trough-1row-25ml
+# 界面
+st.title('连接测试')
 
-p50000 = instruments.Pipette(
-        axis="a",
-        max_volume=50000,
-        min_volume=0,
-        channels=1,
-        name="p50000"
-)
+col1, col2, col3 = st.columns(3)
+with col1:
+    port = st.selectbox('请选择端口：', ports)
+    if port!='断开连接':
+        try:
+            #robot.connect(port)
+            switch = True
+        except (FileNotFoundError, WindowsError):
+            pass
+    else:
+        if switch == True:
+            robot.disconnect()
+            switch = False
+with col2:
+    st.header('连接状态：')
+with col3:
+    if not switch:
+        st.image(red, width=70)
+    else:
+        st.image(green, width=70)
 
-wells = []
-wells.extend(tuberack1.wells())
-wells.extend(tuberack2.wells())
-wells.extend(tuberack3.wells())
-wells.extend(tuberack4.wells())
-wells = [well.bottom() for well in wells]
+environment.refresh()
+path = environment.get_path('CALIBRATIONS_FILE')
+st.write('定位文件路径：')
+st.code(path)
 
-p50000.set_speed(dispense=150) #max=1200
-while len(wells)>32:
-    p50000.aspirate(50000, trough.wells('A1'))
-    p50000.delay(2) #延时3秒
-    p50000.dispense(1000, trough.wells('A1').top(-40))
-    for well in wells[0:32]:
-        p50000.dispense(1500, well)
-    p50000.blow_out(trough.wells('A1').top(-40))
-    del wells[0:32]
-if len(wells)<=32 and len(wells)>0:
-    p50000.aspirate(1500*len(wells)+2000, trough.wells('A1'))
-    p50000.delay(3) #延时3秒
-    p50000.dispense(1000, trough.wells('A1').top(-40))
-    for well in wells:
-        p50000.dispense(1500, well)
-    p50000.blow_out(trough.wells('A1').top(-40))
-
-p50000.set_speed(dispense=600)
-p50000.mix(1, 2500, trough.wells('A1').top(-40))
-
-robot.home()
-robot.commands()
-st.title('运行完毕')
+col1, col2 = st.columns(2)
+with col1:
+    if switch:
+        if st.button('复位', disabled=False):
+            robot.home()
+            st.write('已复位')
+    else:
+        st.button('复位', disabled=True)
+with col2:
+    with open(path, "rb") as file:
+         btn = st.download_button(
+                 label="下载定位文件",
+                 data=file,
+                 file_name="calibrations.json",
+                 mime="json")
